1bb2778b19ca9af2b78f64123f6c4f16cd652acb
MiPeSa <bgu109@myy.haaga-helia.fi>
2022-11-09 00:36:47 +0200

    Tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {  
+  BrowserRouter,
+  Routes,  
+  Route,  
+  Link
+} from"react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <header className='App-header'>
+      <h1>Welcome to React Router</h1>
+    </header>
+    <BrowserRouter>
+    <div className='App-navi'>
+      <nav>
+        <ul>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/about">About</Link>{' '} 
+            <Link to="/contact">Contact</Link>{' '} 
+        </ul>
+      </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
     </div>
+    </BrowserRouter>
+  </div>
   );
+}
+
+function Home() {
+  return <h2>This is home page</h2>;
+}
+
+function About() {
+  return <h2>This is about page</h2>;
+}
+
+function Contact() {
+  return <h2>Contact address</h2>;
+}
+
+function NotFound() {
+  return <h2>Not Found</h2>;
 }
 
 export default App;
